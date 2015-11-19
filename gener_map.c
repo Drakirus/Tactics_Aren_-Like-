2bcc21f2e359243f20310ee674e00743942eb1fb
Drakirus <pierrescalade@gmail.com>
2015-11-19 20:52:09 +0100

    ligne de vue fonctionelle de 12h a 3h

--- gener_map.c
+++ gener_map.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define i_taille_map 10 //Nous avons convenu que la map sera un carré de taille 10.
+#define i_taille_map 20 //Nous avons convenu que la map sera un carré de taille 10.
 
 
 void initialise_map(int map[i_taille_map][i_taille_map]) //Fonction initialisant la map vide
@@ -53,8 +53,43 @@ void afficher_map(int map[i_taille_map][i_taille_map]) //Fonction affichant la m
 				printf("_ ");
 			else if(map[i][j]==1)
 				printf("* ");
+			else if(map[i][j]==2)
+				printf("@ ");
+			else if(map[i][j]==3)
+				printf("0 ");
 		}
 		printf("\n");
+	}
+}
+
+float getSlope(float f_a, float f_b){
+	if(f_b == 0){
+		return 99;
+	}
+	return f_a/f_b;
+}
+
+void shadowcasting(int map[i_taille_map][i_taille_map], int i_player_x, int i_player_y){
+	int i,j,cx,cy;
+	float f_slope_min, f_slope_max, f_slope;
+	for (i = 0; i < i_taille_map; i++) {
+		for (j = 0; j < i_taille_map; j++) {
+			if (map[i][j] == 1) {
+				f_slope_min = getSlope((j-(i_player_y+0.5))	 , (i_taille_map-1-i)+(i_player_x+0.5));
+				f_slope_max = getSlope((j+(i_player_y+0.5))	 , (i_taille_map-1-i)-(i_player_x+0.5));
+				printf("%f slope min  %f slope max \n", f_slope_min, f_slope_max);
+			  for (cy = i; cy >= 0; cy--) {
+					for (cx = j; cx < i_taille_map; cx++) {
+						if (i_player_x != cx || i_player_x != cy) {
+							f_slope = getSlope((cx), (i_taille_map-1-cy));
+							if((f_slope > f_slope_min) && (f_slope < f_slope_max || (f_slope_max < 0 && cx > i_player_x))){
+								map[cy][cx] = 3;
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
 
@@ -63,7 +98,10 @@ int main()
 	srand(time(NULL));
 	int map[i_taille_map][i_taille_map];
 	initialise_map(map);
-	obstacle(10, map);
+	obstacle(5, map);
+	map[19][0] = 2;
+	afficher_map(map);
+	shadowcasting(map, 0, 0);
 	afficher_map(map);
 	return 0;
 }
