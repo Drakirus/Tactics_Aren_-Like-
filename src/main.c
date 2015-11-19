998992fa1432a3178af2abd704ccc79c897ed6be
Apeul <martinsla@live.fr>
2015-11-19 09:15:28 +0100

    main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Outil.h"
+
+
 int main(int argc, char const *argv[]) {
-  assert(1+1==3);
-  return 0;
+  	int choix_menu;
+	switch(choix_menu){
+		case 0: break;
+		case 1: /*nouvelle partie*/ break;
+		case 2: /*chargement*/ break;
+	}
 }
