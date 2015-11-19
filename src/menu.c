521f79088a963eca973465fa7da1037c8d5752d6
Apeul <martinsla@live.fr>
2015-11-19 09:07:21 +0100

    Test

---
+++ src/menu.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	int choix_menu;
+	switch(menu){
+		case 0: break;
+		case 1: /*nouvelle partie*/ break;
+		case 2: /*chargement*/ break;
+	}
+}
---
+++ src/tour.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define taille_grille 10
+
+void affiche_map(int map[taille_grille][taille_grille]
