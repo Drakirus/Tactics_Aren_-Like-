f9bfe8dde592c30d546d2ee23e0f89d817752eba
Apeul <martinsla@live.fr>
2015-11-19 09:10:52 +0100

    update_tour

--- src/tour.c
+++ src/tour.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define taille_grille 10
 
-void affiche_map(int map[taille_grille][taille_grille]
+void affiche_map(int map[taille_grille][taille_grille]{
+		int i, j;
+		for(i = 0; i < taille_grille ; i++){
+			for(j = 0 ;  j < taille_grille ; j++){
+				printf("%i ", map[i][j]);
+			}
+			printf("\n");	
+		}
+		printf("\n");
+}
+
