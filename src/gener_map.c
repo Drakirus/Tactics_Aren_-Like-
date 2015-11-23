f2a51b7903597d55566ad0bed19f0f019e6970bc
Apeul <martinsla@live.fr>
2015-11-23 14:39:59 +0100

    On répare les conneries de Pierre

--- src/gener_map.c
+++ src/gener_map.c
@@ -6,18 +6,6 @@
 
 #define i_taille_map 20 //Nous avons convenu que la map sera un carré de taille 10.
 
-
-void initialise_map(int map[i_taille_map][i_taille_map]) //Fonction initialisant la map vide
-{	int i,j;
-	for(i=0;i<i_taille_map;i++)
-	{
-		for(j=0;j<i_taille_map;j++)
-		{
-			map[i][j]=0; //0=vide;
-		}
-	}
-}
-
 void obstacle(int i_chance_obstacle, int map[i_taille_map][i_taille_map]) //Fonction insérant les obstacles dans la map. Les obstacles sont placés aléatoirement dans la map.
 {
 	int i,j;
@@ -42,6 +30,18 @@ void obstacle(int i_chance_obstacle, int map[i_taille_map][i_taille_map]) //Fonc
 	}
 }
 
+void initialise_map(int map[i_taille_map][i_taille_map]) //Fonction initialisant la map vide
+{	int i,j;
+	for(i=0;i<i_taille_map;i++)
+	{
+		for(j=0;j<i_taille_map;j++)
+		{
+			map[i][j]=0; //0=vide;
+		}
+	}
+	obstacle(5, map);
+}
+
 void afficher_map(int map[i_taille_map][i_taille_map]) //Fonction affichant la map
 {
 	int i,j;
@@ -51,43 +51,10 @@ void afficher_map(int map[i_taille_map][i_taille_map]) //Fonction affichant la m
 		{
 			if(map[i][j]==0)
 				printf("_ ");
-			else if(map[i][j]==1)
+			else if(map[i][j]==1)//1 : obstacle
 				printf("* ");
-			else if(map[i][j]==2)
-				printf("@ ");
-			else if(map[i][j]==3)
-				printf("0 ");
 		}
 		printf("\n");
-	}
-}
-
-float getSlope(float f_a, float f_b){
-	if(f_b == 0){
-		return 99;
-	}
-	return f_a/f_b;
-}
-
-void shadowcasting(int map[i_taille_map][i_taille_map], int i_player_x, int i_player_y){
-	int i,j,cx,cy;
-	float f_slope_min, f_slope_max, f_slope;
-	for (i = 0; i < i_taille_map; i++) {
-		for (j = 0; j < i_taille_map; j++) {
-			if (map[i][j] == 1) {
-				f_slope_min = getSlope((j-(i_player_y+0.5))	 , (i_taille_map-1-i)+(i_player_x+0.5));
-				f_slope_max = getSlope((j+(i_player_y+0.5))	 , (i_taille_map-1-i)-(i_player_x+0.5));
-				printf("%f slope min  %f slope max \n", f_slope_min, f_slope_max);
-			  for (cy = i; cy >= 0; cy--) {
-					for (cx = j; cx < i_taille_map; cx++) {
-							f_slope = getSlope((cx), (i_taille_map-1-cy));
-							if((f_slope > f_slope_min) && (f_slope < f_slope_max || (f_slope_max < 0 && cx > i_player_x))){
-								map[cy][cx] = 3;
-							}
-					}
-				}
-			}
-		}
 	}
 }
 
@@ -96,10 +63,6 @@ int main()
 	srand(time(NULL));
 	int map[i_taille_map][i_taille_map];
 	initialise_map(map);
-	obstacle(5, map);
-	map[19][0] = 2;
-	afficher_map(map);
-	shadowcasting(map, 0, 0);
 	afficher_map(map);
 	return 0;
 }
