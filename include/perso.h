7abb2c760c15a4dbe452fcd339cb5deda6dbffd8
Mok <Modira.Mok@gmail.com>
2015-11-23 13:59:22 +0100

    Ajout de la création des classes.

---
+++ include/perso.h
@@ -0,0 +1,18 @@
+typedef struct
+{
+	char s_classe[20];
+	int i_HP_max;
+	int i_HP;
+	int i_PA;
+	int i_PM;
+	char c_team
+}t_perso;
+
+t_perso ensemble_perso[3]=
+{
+	{"Guerrier",50,50,3,3,NULL},
+	{"Archer",30,30,3,3,NULL},
+	{"Mage",20,20,2,2,NULL},
+}
+	
+
