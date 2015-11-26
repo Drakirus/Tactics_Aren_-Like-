b919cd14792bcec89e452d4dc12a7be23e703161
unknown <Modira.Mok@gmail.com>
2015-11-26 09:09:39 +0100

    On met en place la liste des persos

--- include/perso.h
+++ include/perso.h
@@ -5,13 +5,14 @@ typedef struct
 	int i_HP;
 	int i_PA;
 	int i_PM;
+	int coord[2];
 	char c_team;
 }t_perso;
 
 t_perso ensemble_perso[3]=
 {
-	{"Guerrier",50,50,3,3,'A'},
-	{"Archer",30,30,3,3,'A'},
-	{"Mage",20,20,2,2,'A'},
+	{"Guerrier",50,50,3,3,[0,0],'A'},
+	{"Archer",30,30,3,3,[0,0],'A'},
+	{"Mage",20,20,2,2,[0,0],'A'},
 };
 
--- src/listes.c
+++ src/listes.c
@@ -1,123 +1,122 @@
-/* Mise en oeuvre contigue d'un tableau de listes d'entiers */
-#define MAX_TAB 100
-#define MAX_LISTE 11
 #include<stdio.h>
 #include<stdlib.h>
 
-/* Definition du type d'un element de liste */
-typedef struct element {int valeur; struct element* pred; struct element* succ;} t_element;
+typedef struct element
+{
+	int valeur;
+	struct element * prec;
+	struct element * suiv;
+} t_element;
 
-/* Declaration des listes (drapeau et element courant) */
-t_element* drapeau[MAX_LISTE];
-t_element* ec[MAX_LISTE];
+t_element * drapeau;
+t_element * ec;
 
+void init_liste(void)
+/* Initialise la liste à vide */
+{	
+	drapeau=(t_element *)malloc(sizeof(t_element));
+	drapeau->suiv=drapeau;
+	drapeau->prec=drapeau;
+	ec=drapeau;
+}
 
-/* Primitives de manipulation des listes */
+int liste_vide(void)
+/* Rend vrai si la liste est vide, faux sinon */
+{	
+	return (drapeau->suiv==drapeau);
+}
 
-void init_liste(int n)
-{	if(n>0&&n<MAX_LISTE)
-	{		
-		drapeau[n] = malloc(sizeof(t_element));
-		drapeau[n]->pred = drapeau[n];
-		drapeau[n]->succ = drapeau[n];
-		ec[n] = drapeau[n];
+int hors_liste(void)
+/* Rend vrai si l'elt courant est hors de la liste, faux sinon */
+{	
+	return(ec==drapeau);
+}
+
+void en_tete(void)
+/* Positionne en tete de la liste */
+{	
+	if(!liste_vide()) 
+		ec=drapeau->suiv;
+}
+
+void en_queue(void)
+/* Positionne en queue de la liste */
+{	
+	if(!liste_vide()) 
+		ec=drapeau->prec;
+}
+
+void precedent(void)
+/* Positionne sur l'elt precedent*/
+{	
+	if(!hors_liste()) 
+		ec=ec->prec;
+}
+
+void suivant(void)
+/* Positionne sur l'elt suivant*/
+{	
+	if(!hors_liste()) 
+		ec=ec->suiv;
+}
+
+void valeur_elt(int* v)
+/* Renvoie dans e la veleur de l'elt courant */
+{	
+	if(!hors_liste()) 
+		*v=ec->valeur;
+}
+
+void modif_elt(int v)
+/* Affecte v à l'elt courant */
+{	
+	if(!hors_liste()) 
+		ec->valeur=v;
+}
+
+void oter_elt(void)
+/* Supprime l'elt courant et positionne sur le precedent */
+{	
+	t_element * temp;
+	if(!hors_liste())
+	{
+		ec->prec->suiv=ec->suiv;
+		ec->suiv->prec=ec->prec;
+		temp=ec;
+		precedent();
+		free(temp);
+	}
+
+}
+
+void ajout_droit(int v)
+/* Ajoute v a droite de l'elt courant */
+{	
+	t_element * temp;
+	if(liste_vide()||!hors_liste())
+	{	
+		temp=(t_element*)malloc(sizeof(t_element));
+		temp->prec=ec;
+		temp->suiv=ec->suiv;
+		temp->valeur=v;
+		ec->suiv->prec=temp;
+		ec->suiv=temp;
+		ec=temp;
 	}
 }
 
-int liste_vide(int n)
-{	if(n>0&&n<MAX_LISTE)
-		return drapeau[n]->pred==drapeau[n];
-	return 0;
-}
-
-int hors_liste(int n)
-{	if(n>0&&n<MAX_LISTE)
-		return ec[n]==drapeau[n];
-	return 0;
-}
-
-void en_tete(int n)
-{	if(n>0&&n<MAX_LISTE)
-		if (!liste_vide(n))
-			ec[n] = drapeau[n]->succ;
-}
-
-void en_queue(int n)
-{	if(n>0&&n<MAX_LISTE)
-		if (!liste_vide(n))
-			ec[n] = drapeau[n]->pred;
-}
-
-void precedent(int n)
-{	if(n>0&&n<MAX_LISTE)
-		if (!hors_liste(n))
-			ec[n] = ec[n]->pred;
-}
-
-void suivant(int n)
-{	if(n>0&&n<MAX_LISTE)
-		if (!hors_liste(n))
-			ec[n] = ec[n]->succ;
-}
-
-void valeur_elt(int n, int *v)
-{	if(n>0&&n<MAX_LISTE)
-		if (!hors_liste(n))
-			*v = ec[n]->valeur;
-}
-
-void modif_elt(int n,int v)
-{	if(n>0&&n<MAX_LISTE)
-		if (!hors_liste(n))
-			ec[n]->valeur = v;
-}
-
-void oter_elt(int n)
+void ajout_gauche(int v)
+/* Ajoute v a gauche de l'elt courant */
 {	
 	t_element * temp;
-
-    	if(n>0&&n<MAX_LISTE)
-		if (!hors_liste(n))
-		{	
-			(ec[n]->succ)->pred = ec[n]->pred;
-			(ec[n]->pred)->succ = ec[n]->succ;
-			temp = ec[n];
-			ec[n] = ec[n]->pred;
-			free(temp);
-		}
+	if(liste_vide()||!hors_liste())
+	{	
+		temp=(t_element*)malloc(sizeof(t_element));
+		temp->suiv=ec;
+		temp->prec=ec->prec;
+		temp->valeur=v;
+		ec->prec->suiv=temp;
+		ec->prec=temp;
+		ec=temp;
+	}
 }
-
-void ajout_droit(int n, int v)
-{	
-	t_element* nouv;
-	
-	if(n>0&&n<MAX_LISTE)
-		if (liste_vide(n) || !hors_liste(n))
-		{	
-			nouv = malloc(sizeof(t_element));
-			nouv->valeur = v;
-			nouv->pred = ec[n];
-			nouv->succ = ec[n]->succ;
-			(ec[n]->succ)->pred = nouv;
-			ec[n]->succ = nouv;
-			ec[n] = nouv;
-		}
-}
-
-void ajout_gauche(int n, int v)
-{	
-	t_element* nouv;
-	
-	if(n>0&&n<MAX_LISTE)
-		if (liste_vide(n) || !hors_liste(n))
-		{	
-			nouv = malloc(sizeof(t_element));
-			nouv->valeur = v;
-			nouv->succ = ec[n];
-			nouv->pred = ec[n]->pred;
-			(ec[n]->pred)->succ = nouv;
-			ec[n]->pred = nouv;
-			ec[n] = nouv;
-		}
-}
-
